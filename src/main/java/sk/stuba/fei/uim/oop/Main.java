bec2e776a5d45a15adfbd40fe72c4f8f0a51ce45
Jakub Kovář <jakub.kovar@stuba.sk>
2021-03-22 13:17:49 +0100

    frame

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,11 @@
 package sk.stuba.fei.uim.oop;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        var okno = new JFrame();
+        okno.setVisible(true);
+        okno.setSize(300, 300);
     }
 }
