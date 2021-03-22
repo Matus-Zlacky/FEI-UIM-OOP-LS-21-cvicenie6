c4f64d8b3913842e040b5b5f610b73497f342b81
Jakub Kovář <jakub.kovar@stuba.sk>
2021-03-22 13:48:20 +0100

    border layout

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -19,10 +19,13 @@ public class Main {
         panel.add(label);
 
         var panel2 = new JPanel();
-        panel2.setLayout(new GridLayout(2, 2));
-        for (int i = 0; i < 4; i++) {
-            panel2.add(new JLabel("p" + i));
-        }
+        panel2.setLayout(new BorderLayout());
+
+        panel2.add(new JLabel("top"), BorderLayout.PAGE_START);
+        panel2.add(new JLabel("bottom"), BorderLayout.PAGE_END);
+        panel2.add(new JLabel("left"), BorderLayout.LINE_START);
+        panel2.add(new JLabel("right"), BorderLayout.LINE_END);
+        panel2.add(new JLabel("center"), BorderLayout.CENTER);
 
         for (int i = 0; i < 12; i++) {
             if (i == 8) {
