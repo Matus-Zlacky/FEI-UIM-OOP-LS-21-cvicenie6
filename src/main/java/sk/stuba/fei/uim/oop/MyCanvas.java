10580487c27f0c15e369d6cead2d0a90c019f1e5
Jakub Kovář <jakub.kovar@stuba.sk>
2021-03-22 14:16:27 +0100

    buffered image

--- src/main/java/sk/stuba/fei/uim/oop/MyCanvas.java
+++ src/main/java/sk/stuba/fei/uim/oop/MyCanvas.java
@@ -1,22 +1,31 @@
 package sk.stuba.fei.uim.oop;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class MyCanvas extends Canvas {
 
+
+
+
     @Override
     public void paint(Graphics g) {
-        g.setColor(Color.BLUE);
-        g.fillRect(100, 150, 100, 100);
+        var image = new BufferedImage(300, 300, BufferedImage.TYPE_INT_RGB);
+        var ramGraphics = image.getGraphics();
 
-        g.setColor(Color.RED);
-        g.fillPolygon(new int[] {100, 200, 150}, new int[] {150, 150, 50}, 3);
+        ramGraphics.setColor(Color.BLUE);
+        ramGraphics.fillRect(100, 150, 100, 100);
 
-        g.setColor(Color.CYAN);
+        ramGraphics.setColor(Color.RED);
+        ramGraphics.fillPolygon(new int[] {100, 200, 150}, new int[] {150, 150, 50}, 3);
+
+        ramGraphics.setColor(Color.CYAN);
         for (int x = 0; x < 2; x++) {
             for (int y = 0; y < 2; y++) {
-                g.fillRect(110 + 45 * x, 160 + 45 * y, 35, 35);
+                ramGraphics.fillRect(110 + 45 * x, 160 + 45 * y, 35, 35);
             }
         }
+
+        g.drawImage(image, 0, 0, null);
     }
 }
